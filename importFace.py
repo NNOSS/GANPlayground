5f5f70e633904441e1f263479c0f426f6e1baae1
CleanestMink126 <gns126@gmail.com>
2018-04-23 21:07:09 -0400

    deepthought compatible

--- importFace.py
+++ importFace.py
@@ -1,16 +1,10 @@
 import os
-import zipfile
-import requests
-import math
 import numpy as np
 from PIL import Image
-from tqdm import tqdm
 
 DEBUG = False
 
 from glob import glob
-from matplotlib import pyplot
-from PIL import Image
 
 # Image configuration
 data_dir = './Models/data'
@@ -67,57 +61,6 @@ def get_batches(batch_size):
         current_index += batch_size
         yield data_batch / IMAGE_MAX_VALUE - 0.5
 
-def get_confirm_token(response):
-  for key, value in response.cookies.items():
-    if key.startswith('download_warning'):
-      return value
-  return None
-
-def save_response_content(response, destination, chunk_size=32*1024):
-  total_size = int(response.headers.get('content-length', 0))
-  with open(destination, "wb") as f:
-    for chunk in tqdm(response.iter_content(chunk_size), total=total_size,
-		unit='B', unit_scale=True, desc=destination):
-      if chunk: # filter out keep-alive new chunks
-        f.write(chunk)
-
-def download_file_from_google_drive(id, destination):
-  print("Downloading into ./data/... Please wait.")
-  URL = "https://docs.google.com/uc?export=download"
-  session = requests.Session()
-
-  response = session.get(URL, params={ 'id': id }, stream=True)
-  token = get_confirm_token(response)
-
-  if token:
-    params = { 'id' : id, 'confirm' : token }
-    response = session.get(URL, params=params, stream=True)
-
-  save_response_content(response, destination)
-  print("Done.")
-
-def download_celeb_a():
-  dirpath = './Models/data'
-  data_dir = 'celebA'
-  if os.path.exists(os.path.join(dirpath, data_dir)):
-    print('Found Celeb-A - skip')
-    return
-
-  filename, drive_id  = "img_align_celeba.zip", "0B7EVK8r0v71pZjFTYXZWM3FlRnM"
-  save_path = os.path.join(dirpath, filename)
-
-  if os.path.exists(save_path):
-    print('[*] {} already exists'.format(save_path))
-  else:
-    download_file_from_google_drive(drive_id, save_path)
-
-  if not DEBUG:
-    zip_dir = ''
-    with zipfile.ZipFile(save_path) as zf:
-      zip_dir = zf.namelist()[0]
-      zf.extractall(dirpath)
-    os.remove(save_path)
-    os.rename(os.path.join(dirpath, zip_dir), os.path.join(dirpath, data_dir))
 
 def images_square_grid(images, mode='RGB'):
     """
@@ -143,4 +86,4 @@ def images_square_grid(images, mode='RGB'):
 if __name__ == "__main__":
 
     test_images = get_batch(glob(os.path.join(data_dir, 'celebA/*.jpg'))[:10], 56, 56)
-    pyplot.imshow(helper.images_square_grid(test_images))
+    # pyplot.imshow(helper.images_square_grid(test_images))
